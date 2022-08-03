fc74f9a92925f0ed6a43e1f9fdee8c9149d81cbd
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2022-08-03 18:22:31 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -199,7 +199,7 @@ function App() {
         flex={1}
         ai={"center"}
         style={{ padding: 24, backgroundColor: "var(--primary)" }}
-        image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.png" : null}
+        image={CONFIG.SHOW_BACKGROUND ? "/config/images/bg.gif" : null}
       >
         <StyledLogo alt={"logo"} src={"/config/images/logo.png"} />
         <s.SpacerSmall />
